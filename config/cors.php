00eb42dd8b69d593f1ec88c3de5b9bfd657ebe25
alexzozenko <o.o.zozenko@student.csn.khai.edu>
2026-03-13 21:29:05 +0200

    Correct from deploy

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => [env('FRONTEND_URL', 'https://finflow.com.ua')],
 
     'allowed_origins_patterns' => [],
 
